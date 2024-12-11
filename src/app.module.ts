4a2750d85d338c65732a014d13bbd6179ced1d33
Elbaraa Radwan <77853@must.edu.eg>
2024-12-11 17:42:46 +0200

    (setup project structure/generating module) & installing dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
+import { ShowtimeModule } from './showtime/showtime.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, MovieModule, ShowtimeModule, ReservationModule],
   controllers: [],
   providers: [],
 })
